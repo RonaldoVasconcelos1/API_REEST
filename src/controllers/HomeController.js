9b850d368b39371d88c723aeff64f9d11fb01950
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-02-16 22:59:20 -0300

    recreate apresentation from API REST

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,10 @@
 class HomeController {
   index(req, res) {
-    res.json({title: 'Olá'})
+    res.json({
+      method: 'Get',
+      apresentation: 'Hello, welcome to API_REST',
+      author: 'Ronaldo Vasconcelos the computer science student'
+    })
   }
 }
 export default new HomeController()

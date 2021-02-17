cec3dd5eecbcc3b1a570c0c0431f44578256a5c0
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-02-17 16:05:06 -0300

    create method view

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -9,10 +9,23 @@ class User {
       return o_error
     }
   }
+
   async create(o_user) {
     try {
       const o_response = await db('users').insert(o_user);
       return o_response
+
+    } catch (o_error) {
+        return o_error
+    }
+  }
+
+  async view(userId) {
+    try {
+      const o_user = await db('users')
+      .where({ id : userId })
+      .first()
+      return o_user
 
     } catch (o_error) {
         return o_error

d8bd7118335c11c295bcc22d5e7d988ac311c299
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-02-17 15:24:00 -0300

    create method list

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,6 +1,14 @@
 import db from '../database/index'
 
 class User {
+  async list() {
+    try {
+      const o_users = await db('users').select('*')
+      return o_users
+    } catch (o_error) {
+      return o_error
+    }
+  }
   async create(o_user) {
     try {
       const o_response = await db('users').insert(o_user);

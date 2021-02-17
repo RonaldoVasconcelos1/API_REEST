0b3f2e1dd3d29c193e6c6575d2c1266c34423476
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-02-17 13:53:26 -0300

    create method insert

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,23 +1,37 @@
-import userModel from './UserController'
+import userModel from '../models/UserModel'
+import encryptPassword from '../utils/Bcrypt'
 
 class User {
-  static async list(o_request, o_response) {
+   async list(o_request, o_response) {
     return o_response.json({method: 'list'})
   }
 
-  static async view(o_request, o_response) {
+   async view(o_request, o_response) {
     return o_response.json({method: 'view'})
   }
 
-  static async viewByEmail(o_request, o_response) {
+   async viewByEmail(o_request, o_response) {
     return o_response.json({method: 'Email'})
   }
 
-  static async insert(o_request, o_response) {
-    return o_response.json({method: 'insert'})
+  async insert(o_request, o_response) {
+    try {
+      const o_user = o_request.body
+      if(!o_user.nome) return o_response.status(400).end('Name Field is empty')
+      if(!o_user.email) return o_response.status(400).end('Email Field is empty')
+      if(!o_user.password) return o_response.status(400).end('password Field is empty')
+      o_user.password = encryptPassword(o_user.password)
+      const o_userCreate = await userModel.create(o_user)
+      if(o_userCreate.constraint === 'users_email_unique') return o_response.status(400).end('Registered email')
+      return o_response.status(204).end()
+
+    } catch (o_error) {
+      console.log(o_error)
+      return o_response.status(400).end()
+    }
   }
 
-  static async update(o_request, o_response) {
+  async update(o_request, o_response) {
     return o_response.json({method: 'update'})
   }
 }

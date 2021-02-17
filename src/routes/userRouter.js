49736fdf16209c9b54be2b41e0012555f11c7bca
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-02-17 16:06:53 -0300

    create method view and model

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,9 +4,8 @@ import userController from '../controllers/UserController'
 const router = new Router()
 
 router.get('/users', userController.list)
-router.get('/users', userController.view)
+router.get('/users/:id', userController.view)
 router.post('/users', userController.insert)
-router.patch('/users', userController.update)
 
 
 export default router
